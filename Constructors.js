039f24b12fff063da956f27f031098876b0ada6e
Sheilla Shojaie <sheilla.shojaie@utoronto.ca>
2014-05-31 17:13:58 -0400

    Added setManager and fundRequest

--- Constructors.js
+++ Constructors.js
@@ -3,7 +3,7 @@ var SPORTLIST = ["Archery", "Badminton", "Baseball", "Basketball", "Dance", "Dod
 					"Table tennis", "Taekwondo", "Tennis", "Volleyball", "Wrestling"];
 
 function Person(name, age, gender, address, income, sportsInterests, photo,
-	description, isManager, email, phone, requests) {
+	description, isManager, teams, email, phone, requests) {
 	this.name = name;
 	this.age = age;
 	this.gender = gender; //M, F, N
@@ -13,23 +13,21 @@ function Person(name, age, gender, address, income, sportsInterests, photo,
 	this.photo = photo;
 	this.description = description;
 	this.isManager = isManager;
-	this.teams = []; //list of Team objects
+	this.teams = teams; //list of Team objects
 	this.email = email; //private
 	this.phone = phone; //private
 	this.requests = requests; //list of Request objects
 	}
 	
-function Request(amount, endDate, isFunded, mailingList, donorList) {
+function Request(amount, endDate, mailingList, donorList) {
 	this.amount = amount;
-	this.sofar = 0;
 	this.endDate = endDate;
-	this.isFunded = isFunded;
 	this.mailingList = mailingList; //list of donor emails
 	this.donorList = donorList; //list of donor names
 	}
 
 function Team(teamName, ageRange, gender, maxEnrollment, teamManager, minEntryFee, homeRec, 
-	description, photo, mailingList, contactEmail, sports, members) {
+	description, photo, mailingList, contactEmail, sports) {
 	this.teamName  = teamName;
 	this.ageRange = ageRange; // tuple of youngest and oldest age, inclusive
 	this.gender = gender; //M, F, N
@@ -42,58 +40,6 @@ function Team(teamName, ageRange, gender, maxEnrollment, teamManager, minEntryFe
 	this.mailingList = mailingList; //list of donor emails
 	this.contactEmail = contactEmail; //email of teamManager
 	this.sports = sports; //list of keywords from SPORTLIST
-	this.members = []; //list of Person objects
-	}
-
-function addMember(team, newMember) {
-	if (team.members.length == team.maxEnrollment) {
-		var message = team.teamName + "is already at maximum capacity. Try contacting the manager " +
-		"to join a waitlist.";
-		alert(message);
-		}
-	else {
-		newMember.teams.push(team);
-		team.members.push(newMember);
-		var message = "You have successfully joined " + team.teamName + ". Now awaiting sponsorship!";
-		alert(message); 
-		}
 	}
 	
-function removeMember(team, member) {
-	var index = team.members.indexOf(member);
-	if (index > -1) {
-		team.members.splice(index, 1);
-		}
-	var index2 = member.teams.indexOf(team);
-	if (index2 > -1) {
-		member.teams.splice(index2, 1);
-		}
-	message = "You have been removed from " + team.teamName;
-	alert(message);
-	}
-	
-function setManager(team, member) {
-	member.isManager = 1;
-	team.teamManager = member;
-	team.contactEmail = member.email;
-	}
-	
-function fundRequest(request, donationAmount, donorName, donorEmail) {
-	request.soFar += donationAmount;
-	request.mailingList.push(donorEmail);
-	request.donorList.push(donorName);
-	if (soFar >= amount) {
-		request.isFunded = 1;
-		// email donors!	
-		}
-	}
-	
-
-	
-
-
-
-
-
-
 	
