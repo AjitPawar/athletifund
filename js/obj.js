ccc166a48947e361cf34a55e972dcb1c2ba001bc
AjitPawar <apps@pawar.ca>
2014-06-01 05:41:07 -0400

    modals done

--- js/obj.js
+++ js/obj.js
@@ -3,6 +3,14 @@ var SPORTLIST = ["Archery", "Badminton", "Baseball", "Basketball", "Dance", "Dod
 					"Table tennis", "Taekwondo", "Tennis", "Volleyball", "Wrestling"];
 var PEOPLE = [];
 var TEAMS = [];
+var ZIPCODES = new Array();
+
+
+function Zipcode(lat, lon){
+	this.lat = lat;
+	this.lon = lon;
+}
+
 
 function Person(name, age, gender, zipcode, income, sportsInterests, photo,
 	description, isManager, email, phone, requests) {
@@ -19,8 +27,8 @@ function Person(name, age, gender, zipcode, income, sportsInterests, photo,
 	this.email = email; //private
 	this.phone = phone; //private
 	this.requests = requests; //list of Request objects
-	this.lat = lat;
-	this.lon = lon;
+	this.amount = 0;
+	this.amountNeed = 0;
 	}
 
 	
@@ -52,19 +60,13 @@ function Team(teamName, ageRange, gender, maxEnrollment, teamManager, minEntryFe
 
 function addMember(team, newMember) {
 	if (team.members.length == team.maxEnrollment) {
-		var message = team.teamName + "is already at maximum capacity. Try contacting the manager " +
-		"to join a waitlist.";
+		var message = team.teamName + " team is already at maximum capacity.";
 		alert(message);
 		}
-	else if ((team.gender != "N") && (team.gender!= newMember.gender)) ||
-	 (newMember.age > team.ageRange[1]) || (newMember.age < team.ageRange[0]) {
-		var message = "You do not meet the criteria of age or gender to join " + team.teamName;
-		alert(message);
-		}	
 	else {
 		newMember.teams.push(team);
 		team.members.push(newMember);
-		var message = "You have successfully joined " + team.teamName + ". Now awaiting sponsorship!";
+		var message = "You have successfully joined team " + team.teamName;
 		alert(message); 
 		}
 	}
@@ -129,7 +131,17 @@ function search(searchText) {
 
 
 // **** TEST DATA ****
-/*
+
+// Populate zipcodes
+var zip1 = new Zipcode(43.702762, -79.293289);
+var zip2 = new Zipcode(43.788995, -79.301857);
+var zip3 = new Zipcode(43.764560, -79.245409);
+ZIPCODES["M4B2K1"] = zip1;
+ZIPCODES["M1T2M5"] = zip2;
+ZIPCODES["M4B1B3"] = zip3;
+
+
+
 var ls_teams = [];
 var ls_teams2 = [];
 var ls_teams3 = [];
@@ -142,38 +154,34 @@ var sports5_ls = ["Basketball"];
 var mailing_ls = ["generous@bye.com"];
 var mailing2_ls = ["cashmoney@bye.com"];
 
-var person1 = new Person("A", 10, "F", "L5B2Y7", 3.50, sports3_ls, "img/D.jpg", "I'm some girl", 
-	0, ls_teams, "hi@bye.com", "4152232211", ls_requests);
+var person1 = new Person("A", 10, "F", "M4B2K1", 3.50, sports3_ls, "img/D.jpg", "I'm some girl", 
+	0, ls_teams, "hi@bye.com", "4152232211", ls_requests, 0, 120);
 
-var person2 = new Person("B", 12, "F", "L5B2Y7", 1000, sports3_ls, 
-	"img/D.jpg", "I'm some girl also", 1, ls_teams, "hello@bye.com", "1231231233",
-	ls_requests);
+var person2 = new Person("B", 12, "F", "M1T2M5", 1000, sports3_ls, "img/D.jpg", "I'm some girl also", 
+	1, ls_teams, "hello@bye.com", "1231231233",	ls_requests, 0, 60);
 
-var person3 = new Person("C", 15, "M", "L5B2Y7", 3.50, sports5_ls, 
-	"img/D.jpg", "nice guy", 0, ls_teams2, "C@bye.com", "4152232211",
-	ls_requests);
+var person3 = new Person("C", 15, "M", "M4B2K1", 3.50, sports5_ls, "img/D.jpg", "nice guy", 
+	0, ls_teams2, "C@bye.com", "4152232211", ls_requests, 0, 80);
 
-var person4 = new Person("D", 16, "F", "L5B2Y7", 3.50, sports5_ls, 
-	"img/D.jpg", "kool guy", 1, ls_teams2, "D@bye.com", "1111111111",
-	ls_requests);
+var person4 = new Person("D", 16, "F", "M4B1B3", 3.50, sports5_ls, "img/D.jpg", "kool guy", 
+	1, ls_teams2, "D@bye.com", "1111111111", ls_requests, 0, 90);
 
-var person5 = new Person("E", 16, "M", "L5B2Y7", 3.50, sports5_ls, 
-	"img/D.jpg", "dude guy", 0, ls_teams2, "E@bye.com", "2222222222",
-	ls_requests);
+var person5 = new Person("E", 16, "M", "M1T2M5", 3.50, sports5_ls, "img/D.jpg", "dude guy", 
+	0, ls_teams2, "E@bye.com", "2222222222", ls_requests, 0, 50);
 
-var person6 = new Person("F", 18, "M", "L5B2Y7", 3.50, sports5_ls, 
-	"img/D.jpg", "hi guy", 0, ls_teams3, "F@bye.com", "3333333333",
-	ls_requests);
+var person6 = new Person("F", 18, "M", "M4B1B3", 3.50, sports5_ls, "img/D.jpg", "hi guy", 
+	0, ls_teams3, "F@bye.com", "3333333333", ls_requests, 0, 180);
+
 
 var membership_ls = [person1,person2];
 var membership2_ls = [person3,person4, person5];
 
-var team1 = new Team("Preteen Girls' Rugby", (9,13), "F", 25,
+var team1 = new Team("Warriors", "Rugby", (9,13), "F", 25,
  person2, 25, "recreationcentre1", "small people. violent sports.",
   "img/friendship.jpg", mailing_ls,	"hello@bye.com", sports2_ls, membership_ls);
 
 
-var team2 = new Team("Dancing Queens", (15,18), "N", 3, person4,
+var team2 = new Team("Little League", (15,18), "N", 3, person4,
 	5, "recplace", "sparkley", "teamsportz.jpg", mailing2_ls,
 	"img/friendship.jpg", sports_ls, membership2_ls);
 
@@ -198,4 +206,4 @@ PEOPLE.push(person6);
 
 TEAMS.push(team1);
 TEAMS.push(team2);
-*/
+
